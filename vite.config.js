b62319cf84239f38165740d3d4cc1da3317e5a0d
omari <shanomari611@gmail.com>
2025-11-26 12:52:47 +0300

    run npm run build

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,41 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     lib: {
+//       entry: 'src/index.js',
+//       name: 'FabCityWidget',
+//       formats: ['umd'],
+//       fileName: (format) => `fab-city-widget.${format}.js`
+//     },
+//     rollupOptions: {
+//       external: [],
+//       output: {
+//         // Provide global variables to use in the UMD build for externalized deps
+//         globals: {}
+//       }
+//     }
+//   },
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://fab-city-express-1.onrender.com',
+//         changeOrigin: true,
+        
+//       }
+//     },
+//     allowedHosts: true
+//   }
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    lib: {
-      entry: 'src/embed.js',
-      name: 'FabCityWidget',
-      formats: ['umd'],
-      fileName: (format) => `fab-city-widget.${format}.js`
-    },
-    rollupOptions: {
-      external: [],
-      output: {
-        // Provide global variables to use in the UMD build for externalized deps
-        globals: {}
-      }
-    }
-  },
   server: {
     proxy: {
       '/api': {
